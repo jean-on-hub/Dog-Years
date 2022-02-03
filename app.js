5a050cc7d5f147dd8b7d2b71757f8c0485485bd1
k2908 <56277386+k2908@users.noreply.github.com>
2022-02-03 08:33:27 +0000

    PROJECT

---
+++ app.js
@@ -0,0 +1,18 @@
+// create variable myAge
+let myAge = 5;
+// create variable earlyYears
+let earlyYears = 2;
+// multiply earlyYears by 10.5
+earlyYears *= 10.5;
+// remove 2 from myAge
+let laterYears = myAge-2;
+// multiply laterYears by 10.5
+laterYears *=4
+// result so far
+console.log(laterYears +" "+earlyYears)
+// calculate age in dog years
+let myAgeInDogYear = earlyYears + laterYears
+// saving my name
+let myName = "jay".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYear} years old in dog years.`)
